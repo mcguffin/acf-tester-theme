33c62b295962f51ca15964c13402e775ee33b107
mcguffin <joern@flyingletters.com>
2018-07-21 08:39:49 +0200

    add post types

--- functions.php
+++ functions.php
@@ -240,6 +240,11 @@ require get_template_directory() . '/inc/template-functions.php';
 require get_template_directory() . '/inc/customizer.php';
 
 /**
+ * Post Types.
+ */
+require get_template_directory() . '/inc/post-types.php';
+
+/**
  * Load Jetpack compatibility file.
  */
 if ( defined( 'JETPACK__VERSION' ) ) {
---
+++ inc/post-types.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+function acf_tester_register_post_types() {
+	var_dump("rgfg");exit();
+	register_post_type('books',array(
+		'label'			=> 'Books',
+		'public'		=> true,
+		'has_archive'	=> true,
+		'menu_position'	=> 25,
+	));
+
+
+	register_post_type('people',array(
+		'label'			=> 'People',
+		'public'		=> true,
+		'has_archive'	=> true,
+		'menu_position'	=> 30,
+	));
+
+
+}
+
+
+add_action( 'init', 'acf_tester_register_post_types' );
